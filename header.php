32595b58a3c25334a92cd07af725ae77506573b6
amrister <amr.ister20@gmail.com>
2019-05-31 15:37:11 +0200

    Base Code For Theme

---
+++ header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>First Experience</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/master.css">
+  </head>
+  <body>
